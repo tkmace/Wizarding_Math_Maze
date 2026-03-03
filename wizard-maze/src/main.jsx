0228cf507d7f6d91dfc93c480565cf533b5ef984
tkmace <tom@fremontbrewing.com>
2026-03-02 19:43:27 -0800

    Delete wizard-maze directory

--- wizard-maze/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import WizardMathMaze from './WizardMathMaze.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WizardMathMaze />
-  </React.StrictMode>
-)
