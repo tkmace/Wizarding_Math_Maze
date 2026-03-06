d0e8f71954db985c1eac0ad3bbc9157dce41062f
tkmace <tom@fremontbrewing.com>
2026-03-05 22:12:10 -0800

    Add files via upload

---
+++ wizard-math-maze/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WizardMaze from './WizardMaze.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WizardMaze />
+  </React.StrictMode>
+)
